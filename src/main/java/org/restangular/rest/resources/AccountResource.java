3b68423bef08e462d9bd4c4bb68673be052d698a
oussama mahfoudh <mohamedoussama.mahfoudh@gmail.com>
2015-04-17 23:50:16 +0100

    Adds AccountResource

---
+++ src/main/java/org/restangular/rest/resources/AccountResource.java
@@ -0,0 +1,58 @@
+package org.restangular.rest.resources;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.ResourceSupport;
+
+/**
+ * Created by oussama on 4/17/2015.
+ */
+public class AccountResource  extends ResourceSupport {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Boolean enabled;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+}
