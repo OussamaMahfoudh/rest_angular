4f7abb01d8e7c60aeb4df110d0ba7273e15d6a09
oussama mahfoudh <mohamedoussama.mahfoudh@gmail.com>
2015-04-22 22:13:23 +0100

    Adds find by methods to repositories.

--- src/main/java/org/restangular/core/repositories/BlogEntryRepository.java
+++ src/main/java/org/restangular/core/repositories/BlogEntryRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface BlogEntryRepository extends JpaRepository<BlogEntry, Long> {
+    BlogEntry findByTitle(String title);
 }
--- src/main/java/org/restangular/core/repositories/BlogRepository.java
+++ src/main/java/org/restangular/core/repositories/BlogRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface BlogRepository extends JpaRepository <Blog, Long> {
+
+    Blog findByBlogName(String blogName);
 }
